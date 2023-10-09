c375003f51782c9de7875151fc14b3e8599998e9
markoke <markokello55@gmail.com>
2023-10-08 20:12:56 -0400

    "complex query"

--- main.py
+++ main.py
@@ -2,17 +2,42 @@
 Main cli or app entry point
 """
 
-from src.test_cicd import add
-import click
+import psycopg2
+import tabulate
+from tabulate import tabulate
+import os
+
+# Connect to the database
+conn = psycopg2.connect(
+    host=os.environ['PGHOST'],
+    port=os.environ['PGPORT'],
+    user=os.environ['PGUSER'],
+    database=os.environ['PGDATABASE']
+)
+
+cur = conn.cursor()
+cur.execute('''
+SELECT students.name, students.duke_id, 
+            COUNT(enrollments.course_id) AS total_courses, 
+            SUM(CAST(enrollments.grade AS DECIMAL(10,2))) AS total_grade_points, 
+            AVG(CAST(enrollments.grade AS DECIMAL(10,2))) AS score
+FROM students
+INNER JOIN enrollments ON students.id = enrollments.student_id
+GROUP BY students.name, students.duke_id
+ORDER BY score DESC;
+''')
+
+results = cur.fetchall()
 
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+cur.close()
+conn.close()
+headers = ['Name', 'Duke ID', 'Total Courses', 'Total Grade Points', 'Average_Score']
+print(tabulate(results, headers=headers, tablefmt='pipe'))
 
+# Save the results to a CSV file
+with open('results.csv', 'w') as f:
+    f.write('name,duke_id,total_courses,total_grade_points,score\n')
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    for result in results:
+        f.write('{},{},{},{},{}\n'.format(*result))
